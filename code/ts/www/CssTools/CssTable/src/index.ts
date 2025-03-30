89d575bdd23a56763e6f90198b07dfde7cc4c6da
Elias <illia.polianskyi@protonmail.com>
2025-03-30 07:03:01 +0200

    bugfixed .css and .js references in .html examples

--- code/ts/www/CssTools/CssTable/src/index.ts
+++ code/ts/www/CssTools/CssTable/src/index.ts
@@ -1,4 +1,6 @@
-import "@jaisocx-css-table-assets/css-table-main-webpack.css";
+import "@jaisocx-css-clean-start-assets/clean-start-main-webpack.css";
 
-
+/* two example themes  */
+import "@jaisocx-css-clean-start-assets/themes/theme-lightmode/clean-start-theme-lightmode-webpack.css";
+import "@jaisocx-css-clean-start-assets/themes/theme-darkmode/clean-start-theme-darkmode-webpack.css";
 
