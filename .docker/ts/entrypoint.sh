11fa339db8af73c2016e1fe971b42ef6997086f7
ILLIA POLIANSKYI <illia.polianskyi@protonmail.com>
2025-04-18 02:47:31 +0200

    dockerized node service ts refined timeout after stop, and upgraded node ver.

--- .docker/ts/entrypoint.sh
+++ .docker/ts/entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-
-
 ## STARTING Node http-server on port 8083
 cd /var/www/code/ts
 
@@ -10,7 +8,10 @@ if [ ! -e /entrypoint/npm-installed.mark ]; then
     npm install
 fi
 
-npm run server & echo \"$!\" > server.pid && \
+npm run server &  # Start in background
+PID1=$!
+trap "echo 'Shutting down...'; kill $PID1 $PID2; wait $PID1 $PID2" SIGINT SIGTERM
+
 
 
 
@@ -21,13 +22,17 @@ if [ ! -e /entrypoint/npm-installed.mark ]; then
     npm install
 fi
 
-npm start & echo \"$!\" > serverExpress.pid
+npm start &  # Start in background
+PID2=$!
+trap "echo 'Shutting down...'; kill $PID1 $PID2; wait $PID1 $PID2" SIGINT SIGTERM
+
+
+
 
 if [ ! -e /entrypoint/npm-installed.mark ]; then
     touch /entrypoint/npm-installed.mark
 fi
 
-
-
-exec "$@"
-
+# Wait for both to exit (container stays alive as long as they run)
+wait $PID1
+wait $PID2
