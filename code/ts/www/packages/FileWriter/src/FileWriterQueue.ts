a9cf8c4dba4e3d87bb93705b92e743c699b33ce1
ILLIA POLIANSKYI <illia.polianskyi@protonmail.me>
2025-04-18 12:20:32 +0200

    FileWriterQueue is ready coded, however not transpiled neither tested.

--- code/ts/www/packages/FileWriter/src/FileWriterQueue.ts
+++ code/ts/www/packages/FileWriter/src/FileWriterQueue.ts
@@ -16,17 +16,47 @@ export class FileWriterQueue {
   fileWriter: FileWriter;
   isWriting = false;
   namedBitsbufs: any;
-  queue: QueueTask[]|null;
+  queue: (QueueTask|number)[]|null;
   enqueuedId: number;
   workingQueueId: number;
+  queueSizeExtendBy: number;
+
+  // future task for the fine tuning of the queue array growth.
+  // the idea is to estimate the intensity of writing tasks,
+  // the fixed queue array size to have an imagination on RAM usage extensivity,
+  // and when the new task id is over the queue array size,
+  // and the working tasks still left and are writing,
+  // whether to place the new tasks into the slower queueTasks array,
+  // this uses dynamic reallocation algorythm just via .push() without methods for a software engineer 
+  // to enborder precise the queue array growth.
+  //
+  // for now, the fixed size queue array grows automatique on the event,
+  // that the new task id is above queue array size,
+  // and there are still some enqueued tasks set to be written,
+  // and they are not written at this moment.
+  // for the file writing task this would let us know,
+  // the writing task intensity is high,
+  // and the queue array size grows.
+  // toWaitWhenLeftPercentage: number;
+  // toWaitWhenLeftTasksNumber: number;
 
 
 
   constructor( 
     inFileWriter: FileWriter,
-    queueSize: number
+    queueSize: number,
+    extendBy: number
   ) {
-    this.queue = new Array( queueSize ) as QueueTask[];
+
+    if ( extendBy === 0 ) {
+      this.queueSizeExtendBy = 102;
+    }
+
+    if ( queueSize === 0 ) {
+      queueSize = this.queueSizeExtendBy;
+    }
+
+    this.queue = new Array( queueSize ) as (QueueTask|number)[];
     this.enqueuedId = 0;
     this.workingQueueId = 0;
 
@@ -35,16 +65,37 @@ export class FileWriterQueue {
     this.namedBitsbufs = new Object();
   }
 
+  // method to write to the console infos from methods.
   setDebug( inDebug: boolean ): FileWriterQueue {
     this.debug = inDebug;
     return this;
   }
 
-  extendQueue( addTasksNumber: number ): number {
+  // fine tuning mehod
+  setQueueSizeExtendBy( extendBy: number ): FileWriterQueue {
+    this.queueSizeExtendBy = extendBy;
+
+    return this;
+  }
+
+
+
+  /**
+   * @description instantiates the new QueueTasks array of bigger size,
+   *                previous tasks waiting, not written, are copied to this new array.
+   *                When this method called on queue overflow,
+   *                extendBy is set by value this.queueSizeExtendBy: number
+   *                value set in the constructor = 102.
+   *                set method is .setQueueSizeExtendBy( extendBy: number );
+   *                
+   * @param extendBy 
+   * @returns 
+   */
+  extendQueue( extendBy: number ): number {
 
     // @ts-ignore
     let queueSize = this.queue.length;
-    let extendedSize: number = queueSize + addTasksNumber;
+    let extendedSize: number = queueSize + extendBy;
     let extendedQueue: QueueTask[] = new Array( extendedSize ) as QueueTask[];
 
     let queueTask: QueueTask = new Object() as QueueTask;
@@ -64,19 +115,74 @@ export class FileWriterQueue {
     return this.queue.length;
   }
 
-  enqueue( 
-    bitsbufName: string, 
-    range: number[] ) {
 
-    let bitsbuf: Uint8Array = this.namedBitsbufs[bitsbufName];
 
-    if ( range[0] < 0 || range[1] > bitsbuf.byteLength || range[0] >= range[1]) {
-      throw new RangeError("Start or End boundaries are out of bitsbuf size.");
+  cleanupQueue( start: number, end: number ): void {
+
+    if ( start < 0 ) {
+      start = 0;
     }
 
     // @ts-ignore
-    this.queue.push({ bitsbuf, 
-      range });
+    if ( ( end === 0 ) || ( end > ( this.queue.length - 1 ) ) ) {
+      // @ts-ignore
+      end = ( this.queue.length - 1 );
+    }
+
+    let queuTaskId: number = start;
+    const cleanupValue: number = 1;
+    for ( queuTaskId = start; queuTaskId < end; queuTaskId++ ) {
+      // @ts-ignore
+      this.queue[queuTaskId] = cleanupValue;
+    }
+
+  }
+
+
+
+  validateOnEnqueueTask ( 
+    bitsbuf: Uint8Array,
+    range: number[] 
+  ): void {
+    if ( range[0] < 0 || range[1] > bitsbuf.byteLength || range[0] >= range[1] ) {
+      throw new Error("Start or End boundaries are out of bitsbuf size.");
+    }
+
+    // @ts-ignore
+    if ( this.enqueuedId === this.queue.length ) {
+      // @ts-ignore
+      if ( this.workingQueueId === this.queue.length ) {
+        this.workingQueueId = 0;
+        this.enqueuedId = 0;
+      } else {
+        this.extendQueue( this.queueSizeExtendBy );
+      }
+    }
+    
+  }
+
+  enqueue ( 
+    bitsbufName: string, 
+    range: number[] 
+  ): void {
+
+    let bitsbuf: Uint8Array = this.namedBitsbufs[bitsbufName];
+
+    if ( bitsbuf === undefined ) {
+      throw new Error(`Named bits buf "${bitsbufName}" was not set in this queue.`);
+    }
+
+    this.validateOnEnqueueTask (
+      bitsbuf,
+      range
+    );
+
+    // @ts-ignore
+    this.queue[this.enqueuedId] = { 
+      bitsbuf, 
+      range } as QueueTask;
+
+    this.enqueuedId++;
 
     if (!this.isWriting) {
       this.processQueue();
@@ -84,24 +190,29 @@ export class FileWriterQueue {
 
   }
 
+
+
   processQueue(): void {
 
     // @ts-ignore
-    if ( this.queue.length === 0 ) {
-      this.isWriting = false;
-
-      return;
-    }
-
     this.isWriting = true;
 
     // @ts-ignore
-    const { bitsbuf, range } = this.queue.shift()!;
+    const { bitsbuf, range } = this.queue[this.workingQueueId];
 
     this.fileWriter
       .appendToFile( bitsbuf, range )
       .then (
         () => {
+
+          // checks, whether last written was the last task in the queue.
+          if ( this.workingQueueId === this.enqueuedId ) {
+            this.isWriting = false;
+            return;
+          }
+
+          this.workingQueueId++;
+          
           this.processQueue();
         }
       );
---
+++ code/ts/www/packages/FileWriter/src/TaskFileWritingQueue.ts
@@ -0,0 +1,44 @@
+export class TaskFileWritingQueue {
+  #bitsbuf: Uint8Array;
+  #range: number[];
+
+  constructor(
+    inBitsbuf: Uint8Array,
+    inRange: number[]
+  ) {
+    this.#bitsbuf = inBitsbuf;
+    this.#range = inRange;
+  }
+
+  getBitsbuf(): Uint8Array {
+    return this.#bitsbuf;
+  }
+
+  getRange(): number[] {
+    return this.#range;
+  }
+
+  getStart(): number {
+    return this.#range[0];
+  }
+
+  getEnd(): number {
+    return this.#range[1];
+  }
+
+  toObject(): object {
+    return { "bitsbuf": this.#bitsbuf, "range": this.#range };
+  }
+
+  toString(): string {
+    const taskAsObject: object = new Object();
+
+    const bitsbufAsText: string = (new TextDecoder()).decode( this.#bitsbuf );
+    taskAsObject["bitsbuf"] = bitsbufAsText;
+    taskAsObject["range"] = this.#range;
+
+    const taskAsJson: string = JSON.stringify( taskAsObject );
+
+    return taskAsJson;
+  }
+}
