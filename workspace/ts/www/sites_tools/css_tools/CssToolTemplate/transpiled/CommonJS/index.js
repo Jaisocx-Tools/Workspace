6215af5aa5fd34a8082af95e0083e7724792c31c
Elias Polianskyi <jaisocx@proton.me>
2025-08-26 00:31:37 +0200

    template for a css sites tool 2.1.3

---
+++ workspace/ts/www/sites_tools/css_tools/CssToolTemplate/transpiled/CommonJS/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("@CssToolTemplate_MediaAndStyles/CssToolTemplate_main_Webpack.css");
+//# sourceMappingURL=index.js.map
---
+++ workspace/ts/www/sites_tools/css_tools/CssToolTemplate/transpiled/ESNext/index.js
@@ -0,0 +1,2 @@
+import "@CssToolTemplate_MediaAndStyles/CssToolTemplate_main_Webpack.css";
+//# sourceMappingURL=index.js.map
