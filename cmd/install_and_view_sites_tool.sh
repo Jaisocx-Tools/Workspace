143128c13952fcff388f60ae4f426ce0ff291221
Elias Polianskyi <jaisocx@proton.me>
2025-08-31 02:12:31 +0200

    refined cmd script to install sites tool preview

--- cmd/install_and_view_sites_tool.sh
+++ cmd/install_and_view_sites_tool.sh
@@ -18,26 +18,17 @@ npm_package_name_and_ver="${npm_package_name}@${npm_package_ver}"
 cd "${to_place_npm_package_folder_path}"
 npm install -S  --install-strategy=hoisted  --include=optional  "${npm_package_name_and_ver}"
 
-# cd "node_modules/${npm_package_name}"
-# npm install -S --install-strategy hoisted
-
 
 
 cd "${thisPath}"
 
-
-
-# 5. Open the example HTML in the default browser
+# 5. Open the example HTML in a browser
 
 commandname_1="xdg-open"
 is_installed_1="$(whereis "${commandname_1}")"
-echo -e "${is_installed_1}\n"
-
-
 
 commandname_2="open"
 is_installed_2="$(whereis "${commandname_2}")"
-echo -e "${is_installed_2}\n"
 
 
 
@@ -46,9 +37,11 @@ npmlib_example_html_path="${to_place_npm_package_folder_path}/node_modules/${npm
 if [[ "${is_installed_1}" != "${commandname_1}:" ]]; then
   # echo "matched expr with ${commandname_1}"
   "$($commandname_1 "${npmlib_example_html_path}")"
+
 elif  [[ "${is_installed_2}" != "${commandname_2}:" ]]; then
   # echo "matched expr with ${commandname_2}"
   "$($commandname_2 "${npmlib_example_html_path}")"
+
 else
   echo -e "Please open ${NPMLIB_EXAMPLE_HTML_FILENAME} in your browser at path:\n ${npmlib_example_html_path}\n"
 fi
