734b73026b193d968b69f8a3b72497b1f032ec24
Elias Polianskyi <jaisocx@proton.me>
2025-09-25 10:37:14 +0200

    fonts packages improvals 2

--- workspace/ts/www/sites_tools/media_tools/JscFonts_Newsreader/transpiled/CommonJS/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("@JscFonts_Newsreader_MediaAndStyles/JscFonts_Newsreader_main_Webpack_minimal.css");
-//# sourceMappingURL=index.js.map
--- workspace/ts/www/sites_tools/media_tools/JscFonts_Newsreader/transpiled/ESNext/index.d.ts
+++
@@ -1,2 +0,0 @@
-import "@JscFonts_Newsreader_MediaAndStyles/JscFonts_Newsreader_main_Webpack_minimal.css";
-//# sourceMappingURL=index.d.ts.map
